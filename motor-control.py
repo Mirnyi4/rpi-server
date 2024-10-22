934366bd63ce618cdc9b3f91fe0822e8b10dacd4
Mirnyi4 <teti250801teti@gmail.com>
2024-10-22 15:27:19 +0300

    Update motor-control.py

--- motor-control.py
+++ motor-control.py
@@ -1,33 +1,32 @@
-from gpiozero import Motor, PWMOutputDevice
-from time import sleep
+import RPi.GPIO as GPIO
+import time
 
-# Настраиваем моторы и PWM-выходы для управления скоростью
-motor1 = Motor(17, 27)  # IN1 и IN2 для первого мотора
-motor2 = Motor(22, 23)  # IN3 и IN4 для второго мотора
+# Настройка GPIO
+GPIO.setmode(GPIO.BCM)
+motor_pins = {
+    'forward': 17,
+    'backward': 18,
+    'left': 27,
+    'right': 22,
+}
 
-ena = PWMOutputDevice(12)  # ENA для регулировки скорости первого мотора
-enb = PWMOutputDevice(13)  # ENB для второго мотора
+# Настройка пинов для моторов
+for pin in motor_pins.values():
+    GPIO.setup(pin, GPIO.OUT)
 
-# Устанавливаем скорость (0.0 - 1.0)
-ena.value = 0.8
-enb.value = 0.8
+def check_motors():
+    for direction, pin in motor_pins.items():
+        print(f"Проверка мотора {direction}...")
+        GPIO.output(pin, GPIO.HIGH)  # Включаем мотор
+        time.sleep(1)  # Ждем 1 секунду
+        GPIO.output(pin, GPIO.LOW)   # Выключаем мотор
+        print(f"Мотор {direction} проверен.")
 
-# Команды для управления моторами
-print("Двигатели вперёд")
-motor1.forward()
-motor2.forward()
-sleep(2)
-
-print("Стоп")
-motor1.stop()
-motor2.stop()
-sleep(1)
-
-print("Двигатели назад")
-motor1.backward()
-motor2.backward()
-sleep(2)
-
-print("Стоп")
-motor1.stop()
-motor2.stop()
+try:
+    while True:
+        check_motors()
+        time.sleep(2)  # Задержка перед следующей проверкой
+except KeyboardInterrupt:
+    print("Остановка проверки двигателей.")
+finally:
+    GPIO.cleanup()  # Очистка настроек GPIO
