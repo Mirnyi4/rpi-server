defa12558dd797434ba1e87e4a87e9d641322ee6
Mirnyi4 <teti250801teti@gmail.com>
2024-10-23 19:40:57 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,8 @@
+import subprocess
+import numpy as np
 from flask import Flask, render_template, Response
 from flask_socketio import SocketIO
 from gpiozero import Motor, PWMOutputDevice
-import time
-import numpy as np
-import subprocess
-import cv2
 
 # Настраиваем Flask и веб-сокеты
 app = Flask(__name__)
@@ -16,30 +14,28 @@ motor2 = Motor(22, 23)
 ena = PWMOutputDevice(12)
 enb = PWMOutputDevice(13)
 
-# Настраиваем камеру с использованием Libcamera
+# Главная страница
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Генератор видео-потока
 def gen():
-    # Запуск команды Libcamera с выводом в формате H264
     process = subprocess.Popen(
         ['libcamera-vid', '--inline', '--width', '640', '--height', '480', '--framerate', '30', '--output', 'stdout'],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE
     )
-
+    
     while True:
-        # Чтение данных из stdout
         raw_frame = process.stdout.read(640 * 480 * 3)  # Чтение одного кадра
         if not raw_frame:
             break
-        # Конвертация полученного буфера в изображение
         frame = np.frombuffer(raw_frame, dtype=np.uint8).reshape((480, 640, 3))
         _, buffer = cv2.imencode('.jpg', frame)
         frame = buffer.tobytes()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 @app.route('/video_feed')
 def video_feed():
