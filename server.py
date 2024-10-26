2e8a8d2672a0702a2bcc93419107fa77eccdc2dc
Mallard-rc <teti250801teti@gmail.com>
2024-10-26 15:35:28 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,55 +1,56 @@
 import asyncio
-import websockets
-from aiortc import RTCPeerConnection, VideoStreamTrack
-from aiortc.contrib.media import MediaPlayer
+from aiortc import RTCPeerConnection, RTCSessionDescription, VideoStreamTrack
+from aiortc.contrib.media import MediaBlackhole, MediaPlayer
 import RPi.GPIO as GPIO
+from aiohttp import web
 
-# Настраиваем GPIO
-pins = [12, 14, 16, 17]
+# Настройки пинов для двигателей
+motor_pins = {"left": 12, "right": 14, "forward": 16, "backward": 17}
 GPIO.setmode(GPIO.BCM)
-for pin in pins:
+for pin in motor_pins.values():
     GPIO.setup(pin, GPIO.OUT)
 
 # Функции управления двигателями
-def control_motor(pin, state):
-    GPIO.output(pin, state)
+def control_motor(direction, state):
+    GPIO.output(motor_pins[direction], GPIO.HIGH if state else GPIO.LOW)
 
-# Поток с камеры
-class CameraStreamTrack(VideoStreamTrack):
+# Видео-поток с камеры
+class CameraVideoStream(VideoStreamTrack):
     def __init__(self):
         super().__init__()
-        self.player = MediaPlayer('/dev/video0')
+        self.player = MediaPlayer("/dev/video0", format="v4l2")
 
     async def recv(self):
         frame = await self.player.video.recv()
         return frame
 
-# WebRTC соединение
-async def webrtc_handler(peer: RTCPeerConnection):
-    track = CameraStreamTrack()
-    peer.addTrack(track)
+# Инициализация WebRTC
+pc = RTCPeerConnection()
 
-# WebSocket для управления двигателями
-async def motor_handler(websocket, path):
-    async for message in websocket:
-        if message == "w":
-            control_motor(12, True)
-        elif message == "s":
-            control_motor(12, False)
-        elif message == "a":
-            control_motor(16, True)
-        elif message == "d":
-            control_motor(16, False)
+async def offer(request):
+    params = await request.json()
+    offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
+    await pc.setRemoteDescription(offer)
 
-# Запуск серверов
-async def main():
-    # WebRTC
-    rtc = RTCPeerConnection()
-    rtc_handler = webrtc_handler(rtc)
+    pc.addTrack(CameraVideoStream())
+    answer = await pc.createAnswer()
+    await pc.setLocalDescription(answer)
 
-    # WebSocket
-    websocket_server = websockets.serve(motor_handler, '0.0.0.0', 8000)
+    return web.json_response({
+        "sdp": pc.localDescription.sdp,
+        "type": pc.localDescription.type
+    })
 
-    await asyncio.gather(rtc_handler, websocket_server)
+async def handle_control(request):
+    data = await request.json()
+    direction = data.get("direction")
+    state = data.get("state", False)
+    control_motor(direction, state)
+    return web.Response(text="OK")
 
-asyncio.run(main())
+# Настройка сервера
+app = web.Application()
+app.add_routes([web.post("/offer", offer), web.post("/control", handle_control)])
+
+if __name__ == "__main__":
+    web.run_app(app, host="0.0.0.0", port=8080)
