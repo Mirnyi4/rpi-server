7d4611ef4a0ed0371138aa382a978de520e322f9
Mallard-rc <teti250801teti@gmail.com>
2024-11-08 16:35:09 +0200

    Update server.py

--- server.py
+++ server.py
@@ -8,7 +8,8 @@ pins = {
     'w': 12,  # GPIO 18 - Вперед
     's': 16,  # GPIO 23 - Назад
     'a': 18,  # GPIO 24 - Влево
-    'd': 22   # GPIO 25 - Вправо
+    'd': 22,  # GPIO 25 - Вправо
+    'ena': 8  # GPIO 8 - Управление ENA
 }
 
 # Настраиваем пины как выходы и выключаем их
@@ -24,6 +25,13 @@ def activate_pin(pin):
     time.sleep(0.5)
     GPIO.output(pin, GPIO.LOW)
 
+# Функция для регулирования ENA
+def control_ena(value):
+    if value == 'on':
+        GPIO.output(pins['ena'], GPIO.HIGH)
+    else:
+        GPIO.output(pins['ena'], GPIO.LOW)
+
 # Маршрут для главной страницы
 @app.route('/')
 def index():
@@ -32,9 +40,17 @@ def index():
 # Маршрут для получения команд управления
 @app.route('/control', methods=['POST'])
 def control():
-    direction = request.form['direction']
-    if direction in pins:
-        activate_pin(pins[direction])
+    directions = request.form.getlist('direction')  # Список всех зажатых клавиш
+    for direction in directions:
+        if direction in pins:
+            activate_pin(pins[direction])
+    return '', 204  # Пустой ответ с кодом 204 (успех, без контента)
+
+# Маршрут для управления ENA с ползунка
+@app.route('/control_ena', methods=['POST'])
+def control_ena_route():
+    value = request.form['value']
+    control_ena(value)
     return '', 204  # Пустой ответ с кодом 204 (успех, без контента)
 
 if __name__ == '__main__':
