10187db351f7372a30a5023a309f7d41bdb6b70d
Mallard-rc <teti250801teti@gmail.com>
2024-11-27 16:59:28 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 from flask import Flask, render_template, request
+import time
 
 # Настройка GPIO
 GPIO.setmode(GPIO.BOARD)
@@ -7,7 +8,8 @@ pins = {
     'w': 12,  # GPIO 18 - Вперед
     's': 16,  # GPIO 23 - Назад
     'a': 18,  # GPIO 24 - Влево
-    'd': 22   # GPIO 25 - Вправо
+    'd': 22,  # GPIO 25 - Вправо
+    'ena': 8  # GPIO 8 - Управление ENA
 }
 
 # Настраиваем пины как выходы и выключаем их
@@ -20,10 +22,15 @@ app = Flask(__name__)
 # Функция активации пина
 def activate_pin(pin):
     GPIO.output(pin, GPIO.HIGH)
-
-# Функция деактивации пина
-def deactivate_pin(pin):
+    time.sleep(0.5)
     GPIO.output(pin, GPIO.LOW)
+
+# Функция для регулирования ENA
+def control_ena(value):
+    if value == 'on':
+        GPIO.output(pins['ena'], GPIO.HIGH)
+    else:
+        GPIO.output(pins['ena'], GPIO.LOW)
 
 # Маршрут для главной страницы
 @app.route('/')
@@ -38,15 +45,15 @@ def control():
         activate_pin(pins[direction])
     return '', 204  # Пустой ответ с кодом 204 (успех, без контента)
 
-# Маршрут для остановки движения
-@app.route('/stop', methods=['POST'])
-def stop():
-    for pin in pins.values():
-        deactivate_pin(pin)
+# Маршрут для управления ENA с ползунка
+@app.route('/control_ena', methods=['POST'])
+def control_ena_route():
+    value = request.form['value']
+    control_ena(value)
     return '', 204  # Пустой ответ с кодом 204 (успех, без контента)
 
 if __name__ == '__main__':
     try:
         app.run(host='0.0.0.0', port=5000)
     finally:
-        GPIO.cleanup()  # Очищаем пины при завершении работ
+        GPIO.cleanup()  # Очищаем пины при завершении работы
